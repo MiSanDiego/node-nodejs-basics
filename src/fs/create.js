cd3b70a38d3e90d93101519d97dee97f8558ca10
mikegrekov <svg200914@gmail.com>
2024-10-07 21:29:45 +0100

    feat: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+const content = 'I am fresh and young';
+
+// const create = async () => {
+//     await fs.appendFile('message.txt', content, function (err) {
+//         if (err) throw err;
+//         console.log('Saved!');
+//       });
+// };
 const create = async () => {
-    // Write your code here 
+    await fs.promises.appendFile('message.txt', content);
+    return true;
 };
 
 await create();
