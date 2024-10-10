86dc55c69e7075340cfbc1f79f42ef321d5175d7
mikegrekov <svg200914@gmail.com>
2024-10-10 17:47:04 +0100

    feate: list, read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { promises as fs } from 'fs';
+import {constants} from 'fs';
+
+const errorMessage = 'FS operation failed';
+const fileToRead = './src/fs/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        await fs.access(fileToRead, constants.F_OK);
+        try {
+            fs.readFile(fileToRead, {encoding:"utf-8"}).then((result) => console.log(result));
+        } catch (error) {
+            throw Error (errorMessage);
+        }
+    } catch (error) {
+        throw Error (errorMessage);
+    }
 };
 
 await read();
